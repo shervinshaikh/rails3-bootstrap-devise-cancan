e1f80e18fd4d5add77da73369b36a829659aadff
Shervin Shaikh <shervin.shaikh@appfolio.com>
2013-09-07 12:03:48 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails3BootstrapDeviseCancan::Application.config.secret_key_base = 'a9c67bac3bdae10dc0031ec2753b53b8deffbbd9011576c238c6bf8479ee0d7695059d7c95c762b49901dca8ab15d8d383f6b559b9ccb8d62fdb2b0849da16f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails3BootstrapDeviseCancan::Application.config.session_store :cookie_store, key: '_rails3-bootstrap-devise-cancan_session'
